40f5d1c14f18d6b115b01c9e5fef0940352eb800
murukaimberi <murukai.mberi@gmail.com>
2022-03-13 12:51:27 +0200

    Finished refactoring code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AboutComponent } from './components/about/about.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { ServicesComponent } from './components/services/services.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent },
   {path: 'about-us', component: AboutComponent },
   {path: 'contact-us', component: ContactUsComponent },
   {path: 'projects', component: ProjectsComponent },
+  {path: 'services', component: ServicesComponent },
   {path: '', component: HomeComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ContactUsComponent } from './components/contact-us/contact-us.component
 import { ProjectsComponent } from './components/projects/projects.component';
 import { NavbarComponent } from './navigation/navbar/navbar.component';
 import { FooterComponent } from './navigation/footer/footer.component';
+import { ServicesComponent } from './components/services/services.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { FooterComponent } from './navigation/footer/footer.component';
     ContactUsComponent,
     ProjectsComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
